18bb174dcea8cc8dc9f8d3abafa528ba07e5e02a
Branchetti Alessandro <40342541+brnktt@users.noreply.github.com>
2026-08-15 13:41:22 +0100

    Add: python-dynamic-typing - submission-0

---
+++ Python For Beginners/python-dynamic-typing/submission-0.py
@@ -0,0 +1,10 @@
+variable = 25
+print(type(variable))
+variable = 3.14
+print(type(variable))
+variable = False
+print(type(variable))
+variable = "You"
+print(type(variable))
+variable = [3,4,5]
+print(type(variable))
