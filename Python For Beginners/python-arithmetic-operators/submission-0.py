bcd665b7df5b1ad8fa49320fac0be52050969907
Branchetti Alessandro <40342541+brnktt@users.noreply.github.com>
2026-08-15 13:49:34 +0100

    Add: python-arithmetic-operators - submission-0

---
+++ Python For Beginners/python-arithmetic-operators/submission-0.py
@@ -0,0 +1,7 @@
+a, b, c = 2, 2, 0.5
+sum_of_numbers = a + b + c
+print(sum_of_numbers)
+print(0 - sum_of_numbers)
+product_of_numbers = a * b * c
+print(product_of_numbers)
+print(sum_of_numbers / product_of_numbers)
