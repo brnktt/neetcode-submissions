f7e0cd8620e64a71c69e98644608f943dcc72de0
Branchetti Alessandro <40342541+brnktt@users.noreply.github.com>
2026-08-17 13:18:12 +0100

    Add: python-read-input-practice - submission-2

---
+++ Python For Beginners/python-read-input-practice/submission-2.py
@@ -0,0 +1,18 @@
+def add_two_numbers() -> int:
+    input_string = input()
+    list_string = input_string.split(",")
+    nums = []
+    for num in list_string:
+        nums.append(int(num))
+
+    return sum(nums)
+
+    #return sum([int(num) for num in input().split(",")])
+
+
+
+# do not modify below this line
+print(add_two_numbers())
+print(add_two_numbers())
+print(add_two_numbers())
+print(add_two_numbers())
